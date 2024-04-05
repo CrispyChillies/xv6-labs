10bbce7f9f53752091036ac3f70e60a0a17c8652
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2024-04-05 20:14:41 +0700

    Add missing files causing error to run QEMU

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,39 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Print information of a page table and its lower level page table
+void
+print_pagetable(pagetable_t pagetable, int level)
+{
+  // print all the PTE
+  for (int i = 0; i < 512; ++i)
+  {
+    pte_t pte = pagetable[i];
+    // if this PTE is valid then we print out its info
+    if (pte & PTE_V)  
+    {
+      for (int j = 0; j <= level; ++j)
+      {
+        printf(" ..");
+      }
+      
+      // print the PTE's info
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+    // if this PTE is valid but neither writtable or readable or executable, then it points to another lower-level page table
+    // walk down to the lower level
+    if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0)
+    {
+      uint64 child = PTE2PA(pte); // get the physical address of the child
+      print_pagetable((pagetable_t)child, level + 1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  print_pagetable(pagetable, 0);
+}
